9f6c25a7fad9fcad002075d795c945d3a388a554
ireenna <smarty/maks13@gmail.com>
2022-07-26 15:36:51 +0300

    Class Snake Done

--- Program.cs
+++ Program.cs
@@ -4,72 +4,12 @@ namespace SnackeLastChance
 {
     class Program
     {
-        FructFactory heart = new FructFactory();
-
-        
-
-        
-
-        
-
-        public static int ToCheckMaxValue()
-        {
-            int max = 0;
-
-            for (int i = 0; i < GameSettings.BackGroundField.GetLength(0); i++)
-            {
-                for (int j = 0; j < GameSettings.BackGroundField.GetLength(1); j++)
-                {
-                    if (GameSettings.BackGroundField[i, j] > max)
-                    {
-                        max = GameSettings.BackGroundField[i, j];
-                    }
-                }
-            }
-            return max;
-        }
-
-        public static void ToUpValueBody()
-        {
-            for (int i = 0; i < GameSettings.BackGroundField.GetLength(0); i++)
-            {
-                for (int j = 0; j < GameSettings.BackGroundField.GetLength(1); j++)
-                {
-                    if (GameSettings.BackGroundField[i, j] > 1)
-                    {
-                        GameSettings.BackGroundField[i, j]++;
-                    }
-                }
-            }
-        }
-        public static void ToDeleteMaxValue()
-        {
-            int max = 0;
-            int x = 0;
-            int y = 0;
-
-            for (int i = 0; i < GameSettings.BackGroundField.GetLength(0); i++)
-            {
-                for (int j = 0; j < GameSettings.BackGroundField.GetLength(1); j++)
-                {
-                    if (GameSettings.BackGroundField[i, j] > max)
-                    {
-                        max = GameSettings.BackGroundField[i, j];
-                        x = i;
-                        y = j;
-                    }
-                }
-            }
-
-            GameSettings.BackGroundField[x, y] = 0;
-        }
-
         static void Main()
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             while (Snake.Alive)
             {
-                ToMoveSnake();
+                Snake.ToMoveSnake();
                 FructFactory.ToCreateTheFruict();
                 Teacher.ToDrawBrickInTheWall();
                 ToReadKey();
@@ -84,113 +24,33 @@ namespace SnackeLastChance
             switch (input.Key)
             {
                 case ConsoleKey.LeftArrow:
-                    if (right == false)
+                    if (Snake.Right == false)
                     {
-                        left = true;
-                        up = down = false;
+                        Snake.Left = true;
+                        Snake.Up = Snake.Down = false;
                     }
                     break;
                 case ConsoleKey.RightArrow:
-                    if (left == false)
+                    if (Snake.Left == false)
                     {
-                        right = true;
-                        up = down = false;
+                        Snake.Right = true;
+                        Snake.Up = Snake.Down = false;
                     }
                     break;
                 case ConsoleKey.UpArrow:
-                    if (down == false)
+                    if (Snake.Down == false)
                     {
-                        up = true;
-                        left = right = false;
+                        Snake.Up = true;
+                        Snake.Left = Snake.Right = false;
                     }
                     break;
                 case ConsoleKey.DownArrow:
-                    if (up == false)
+                    if (Snake.Up == false)
                     {
-                        down = true;
-                        left = right = false;
+                        Snake.Down = true;
+                        Snake.Left = Snake.Right = false;
                     }
                     break;
-            }
-        }
-
-        public static void ToMoveSnake()
-        {
-
-            int xHead = 0;
-            int yHead = 0;
-
-            for (int i = 0; i < GameSettings.BackGroundField.GetLength(0); i++)
-            {
-                for (int j = 0; j < GameSettings.BackGroundField.GetLength(1); j++)
-                {
-                    if (GameSettings.BackGroundField[i, j] == 2)
-                    {
-                        xHead = i;
-                        yHead = j;
-                    }
-                }
-            }
-
-            if (up)
-            {
-                if (GameSettings.BackGroundField[xHead - 1, yHead] > 0) alive = false;
-                else
-                {
-                    if (ToCheckMaxValue() > GameSettings.Counter)
-                    {
-                        ToDeleteMaxValue();
-                    }
-
-                    ToUpValueBody();
-                    GameSettings.BackGroundField[xHead - 1, yHead] = 2;
-                }
-
-            }
-
-            else if (down)
-            {
-
-                if (GameSettings.BackGroundField[xHead + 1, yHead] > 0) alive = false;
-                else
-                {
-                    if (ToCheckMaxValue() > GameSettings.Counter)
-                    {
-                        ToDeleteMaxValue();
-                    }
-
-                    ToUpValueBody();
-                    GameSettings.BackGroundField[xHead + 1, yHead] = 2;
-                }
-            }
-
-            else if (right)
-            {
-                if (GameSettings.BackGroundField[xHead, yHead + 1] > 0) alive = false;  
-                else
-                {
-                    if (ToCheckMaxValue() > GameSettings.Counter)
-                    {
-                        ToDeleteMaxValue();
-                    }
-
-                    ToUpValueBody();
-                    GameSettings.BackGroundField[xHead, yHead + 1] = 2;
-                }
-            }
-            else if (left)
-            {
-                if (GameSettings.BackGroundField[xHead, yHead - 1] > 0) alive = false;
-                else
-                {
-                    if (ToCheckMaxValue() > GameSettings.Counter)
-                    {
-                        ToDeleteMaxValue();
-                    }
-
-                    ToUpValueBody();
-                    GameSettings.BackGroundField[xHead, yHead - 1] = 2;
-                }
             }
         }
     }
